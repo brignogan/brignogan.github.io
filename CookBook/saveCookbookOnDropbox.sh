654b166cade4841c654a91a637cd1cf172e090be
ronan <ronan.paugam@gmail.com>
2020-04-21 09:12:53 +0200

    modif avant propos

--- CookBook/saveCookbookOnDropbox.sh
+++ CookBook/saveCookbookOnDropbox.sh
@@ -5,4 +5,4 @@ CURRENTDATEONLY=`date +"%d%b%Y"`
 saved_version='/UnSync/Les Recettes de Coat Tanguy/cookbook_version_'${CURRENTDATEONLY}'.pdf'
 echo $saved_version
 dropbox_uploader.sh upload cookbook.pdf "$saved_version"
-
+dropbox_uploader.sh share "$saved_version"
