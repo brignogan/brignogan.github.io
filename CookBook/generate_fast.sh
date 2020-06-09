1be1f1abe99b0646ddfe346f6b0f1e3ec52610c6
ronan <ronan.paugam@gmail.com>
2020-06-09 17:39:44 +0200

    bash script amelirore

--- CookBook/generate_fast.sh
+++ CookBook/generate_fast.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 #
-source ~/anaconda2/bin/activate cookbook
+DIRECTORY=`dirname $0`
+cd $DIRECTORY
+source $PATH_ANACONDA2/bin/activate cookbook
 python generate_recipe.py --flag_latex False
 python generate_vin.py -s True
 python generate_recipe.py --flag_latex True
