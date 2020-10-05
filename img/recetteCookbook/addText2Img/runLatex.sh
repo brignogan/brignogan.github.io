9e7910d6808edd71b084b72b13525ce13ea0a44e
ronan <ronan.paugam@gmail.com>
2020-10-05 21:45:39 +0200

    nouveau schema

--- img/recetteCookbook/addText2Img/runLatex.sh
+++ img/recetteCookbook/addText2Img/runLatex.sh
@@ -3,7 +3,7 @@ set -o pipefail
 DIRECTORY=`dirname $0`
 cd $DIRECTORY
 [ -e  img/kardinalschnittenSchema.pdf ] && rm img/kardinalschnittenSchema.pdf
-[ -e  img/sachertorteglacage.pdf ] && rm img/sachertorteglacage.pdf
+[ -e  img/sachertorteglacage2.pdf ] && rm img/sachertorteglacage2.pdf
 pdflatex --shell-escape  main.tex
 ln -fs addText2Img/img/kardinalschnittenSchema.pdf ../
-ln -fs addText2Img/img/sachertorteglacage.pdf ../
+ln -fs addText2Img/img/sachertorteglacage2.pdf ../sachertorteglacage.pdf
