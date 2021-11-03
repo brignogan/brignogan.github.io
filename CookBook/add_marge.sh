bf74edfd42d88b3c9081fcbfa0c916b6a1cdc18d
Ronan <ronan.paugam@pm.me>
2021-11-03 12:24:22 +0100

    add_marge.sh

---
+++ CookBook/add_marge.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+pdftk A=cookbook.pdf cat Aeven output tmp_even.pdf 
+pdftk A=cookbook.pdf cat Aodd output  tmp_odd.pdf 
+
+pdfjam --fitpaper true --trim "0 0 -3mm 0" tmp_odd.pdf -o tmp_odd_marge.pdf
+pdfjam --fitpaper true --trim "-3mm 0 0 0" tmp_even.pdf -o tmp_even_marge.pdf
+
+pdftk A=tmp_odd_marge.pdf B=tmp_even_marge.pdf shuffle A B output cookbook_marge.pdf
+rm tmp_*.pdf
--- CookBook/saveCookbookOnDropbox.sh
+++ CookBook/saveCookbookOnDropbox.sh
@@ -2,7 +2,7 @@
 #
 CURRENTDATE=`date +"%Y-%m-%d %T"`
 CURRENTDATEONLY=`date +"%d%b%Y"`
-saved_version='/UnSync/Les Recettes de Coat Tanguy/cookbook_version_'${CURRENTDATEONLY}'.pdf'
+saved_version='/UnSync/Les Recettes de Coat Tanguy/cookbook_marge_version_'${CURRENTDATEONLY}'.pdf'
 echo $saved_version
-dropbox_uploader.sh upload cookbook.pdf "$saved_version"
+dropbox_uploader.sh upload cookbook_marge.pdf "$saved_version"
 dropbox_uploader.sh share "$saved_version"
