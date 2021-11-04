dd1ec5b69c8ec165b56228ff304d5f872d31c554
Ronan <ronan.paugam@pm.me>
2021-11-04 23:23:49 +0100

    avec marge

--- CookBook/saveCookbookOnDropbox.sh
+++ CookBook/saveCookbookOnDropbox.sh
@@ -2,7 +2,14 @@
 #
 CURRENTDATE=`date +"%Y-%m-%d %T"`
 CURRENTDATEONLY=`date +"%d%b%Y"`
-saved_version='/UnSync/Les Recettes de Coat Tanguy/cookbook_marge_version_'${CURRENTDATEONLY}'.pdf'
+
+saved_version='/UnSync/Les Recettes de Coat Tanguy/cookbook_version_'${CURRENTDATEONLY}'.pdf'
 echo $saved_version
-dropbox_uploader.sh upload cookbook_marge.pdf "$saved_version"
+dropbox_uploader.sh upload cookbook.pdf "$saved_version"
 dropbox_uploader.sh share "$saved_version"
+
+saved_version='/UnSync/Les Recettes de Coat Tanguy/cookbook_NoMarge_version_'${CURRENTDATEONLY}'.pdf'
+echo $saved_version
+dropbox_uploader.sh upload cookbook_Nomarge.pdf "$saved_version"
+dropbox_uploader.sh share "$saved_version"
+
