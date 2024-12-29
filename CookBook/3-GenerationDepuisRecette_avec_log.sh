029c7c26e8dcd42c4f334975f30e438f7899a738
ReneePaugam <paugam.renee@gmail.com>
2024-12-29 20:08:56 +0100

    Update 3-GenerationDepuisRecette_avec_log.sh

--- CookBook/3-GenerationDepuisRecette_avec_log.sh
+++ CookBook/3-GenerationDepuisRecette_avec_log.sh
@@ -4,5 +4,5 @@ set -e
 set -o pipefail
 DIRECTORY=`dirname $0`
 cd $DIRECTORY
-source $PATH_ANACONDA2/bin/activate cookbook
+source $PATH_ANACONDA3/bin/activate cookbook
 ./2-GenerationDepuisRecette.sh >& output.txt
